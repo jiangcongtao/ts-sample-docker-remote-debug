7e78b7aa96505e12e25d9c5fa5bd9cd29d7738cb
nickjiang <nick.jiang@hilton.com>
2021-12-08 14:55:21 +0800

    Add yarn run prebuild

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,21 @@ module.exports = {
         commonjs: true,
         es2021: true,
     },
-    extends: ['airbnb-base', 'prettier/@typescript-eslint'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 13,
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                moduleDirectory: ['node_modules', 'src/'],
+            },
+        },
     },
     plugins: ['@typescript-eslint', 'prettier'],
     rules: {
         'no-console': 0,
     },
-}
+};
